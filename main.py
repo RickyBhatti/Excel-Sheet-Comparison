e336b206230906e06764238cd9126a50292f1cc2
RickyBGamez <rickyb5@live.ca>
2024-10-22 23:17:30 -0600

    chore: more basic prep of main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # License: Refer to the LICENSE file.
 
 import os
+import pandas # TODO: Create a requirements.txt file.
 
 # clear_screen: Method to clear the screen.
 clear_screen = lambda: os.system("cls" if os.name == "nt" else "clear")
